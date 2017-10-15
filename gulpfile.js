aaf9b7b5ae3c7e5115a0467279eb8aa2517d0f45
SlashD9 <colin.p.s.daly@gmail.com>
2017-10-15 21:41:26 +0100

    Gulp update, Css BEM

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
 
 gulp.task('default', function(){
     console.log('Hooray - you created a Gulp task');
@@ -10,10 +16,12 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-    console.log("Imagine sass or PostCSS tasks running here.");
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'))
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function(){ 
     watch('./app/index.html', function(){
         gulp.start('html');
     });
